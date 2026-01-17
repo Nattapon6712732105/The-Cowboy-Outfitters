ec8350338ad5b31fae370a1461cdee66524519ce
Nattapon6712732105 <nattaponboonnara@gmail.com>
2026-01-17 14:56:25 +0700

    Update settings for production: disable debug and specify allowed hosts; add WSGI export for Vercel; create .vercelignore file

--- cowboy/settings.py
+++ cowboy/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-&#$@&d3p@mgp*r#uyun)jljds+#uxl$4gnzo+0(z54g1$lt=^y"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '.vercel.app']
 
 
 # Application definition
